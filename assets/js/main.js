2f7b5022d7798a7fa9866c6904998c71eac0e068
Nate Denys <nate_denys@yahoo.com>
2017-09-08 14:02:25 -0700

    create deletion of list item functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-console.log("welcome!")
+// check off specific todos by clicking
+$("li").click(function(){
+	$(this).toggleClass("completed")
+});
+
+
+// click on X to delete Todo
+
+$("span").click(function(event){
+	$(this).parent().fadeOut(function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
